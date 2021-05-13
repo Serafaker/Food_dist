3af4980457eb216db79f9b90f5106546040f6808
Serafaker <77161010+Serafaker@users.noreply.github.com>
2021-05-13 15:52:12 +0300

    Rest operator
    Добавлено добавление класса сss через rest оператор

--- js/script.js
+++ js/script.js
@@ -149,12 +149,13 @@ showTabContent();
 ////Карточки  с использование классов
 
     class MenuCard {
-        constructor(src,alt, title, discr, price, parentSelector){
+        constructor(src,alt, title, discr, price, parentSelector, ...classes){
         this.src= src;
         this.alt= alt;
         this.title= title;
         this.discr= discr;
         this.price= price;
+        this.classes= classes ;
         this.parent= document.querySelector(parentSelector);
         this.transfer= 27;
         this.changeToUAH();
@@ -166,8 +167,13 @@ showTabContent();
 
         render(){
             const element= document.createElement('div');
+            if(this.classes.length===0){
+                this.element= 'menu__item';
+                element.classList.add(this.element); 
+            }else{
+                this.classes.forEach(className => element.classList.add(className));
+            }
             element.innerHTML=`
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.discr}</div>
@@ -176,7 +182,7 @@ showTabContent();
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>`;
+                `;
             this.parent.append(element);
         }
         
@@ -191,7 +197,9 @@ showTabContent();
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+
+       
     ).render();      //Сокращенный вариант, работатет 1 раз далее будет не доступен тк на него отсутвует ссылка 
 
     new MenuCard(
@@ -199,8 +207,9 @@ showTabContent();
         "elite",
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        15,
-        '.menu .container'
+        20,
+        '.menu .container',
+        
     ).render();
 
     new MenuCard(
@@ -209,6 +218,8 @@ showTabContent();
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
         12,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
+    
 });
